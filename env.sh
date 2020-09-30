1032b9499210f54e066b2fc97a3472b8c933ded6
Dexter Haslem <dmh@fastmail.com>
2020-09-30 11:35:20 -0600

    bump versions to latest and greatest

--- env.sh
+++ env.sh
@@ -2,18 +2,18 @@
 
 # this is the cross compile target
 # export TARGET=mips-elf
-# export TARGET=x86_64-elf
-export TARGET=i686-elf
+export TARGET=x86_64-elf
+# export TARGET=i686-elf
 
 # note - make sure the combination of versions work together! see
 # http://wiki.osdev.org/Cross-Compiler_Successful_Builds
-export GCCVER=9.3.0
-export BINUTILSVER=2.34
+export GCCVER=10.2.0
+export BINUTILSVER=2.35.1
 
 # set this to number of threads to compile with, setting this to max cpu
 # (or optionally +1) will greatly decrease the amount of time gcc takes
 # to compile if your cpu is beefy
-export NUMCPU=17
+export NUMCPU=12
 export PREFIX="$HOME/opt/cross"
 export PATH="$PREFIX/bin:$PATH"
 
